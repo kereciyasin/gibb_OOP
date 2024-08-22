fbf43b196865f15b83be892648cdafefe4807182
Yasin Kereci <kereciyasin52@gmail.com>
2024-08-22 09:53:46 +0200

    added

--- Kontrollstrukturen/Kontrollstrukturen/Program.cs
+++ Kontrollstrukturen/Kontrollstrukturen/Program.cs
@@ -6,13 +6,16 @@
 		{
 			Console.WriteLine("Bitte geben Sie einen Satz ein!");
 			string input = Console.ReadLine();
+			// Kelimeleri boşluklardan ayırarak bir diziye atıyoruz.
 			string[] words = input.Split(' ');
+			// Kelime sayısını alıyoruz.
 			int wordCount = words.Length;
 			Console.WriteLine($"Position längstes Word :{wordCount}");
-
+			// En uzun kelimeyi buluyoruz.
 			string longestWord = "";
 			foreach (string word in words)
 			{
+				// Eğer kelimenin uzunluğu en uzun kelimenin uzunluğundan büyükse, en uzun kelimeyi güncelliyoruz.
 				if (word.Length > longestWord.Length)
 				{
 					longestWord = word;
