442befc75d77abd3dc7769e25cc107c96fc84021
Yasin Kereci <kereciyasin52@gmail.com>
2024-08-22 09:52:52 +0200

    added

---
+++ Kontrollstrukturen/Kontrollstrukturen/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Kontrollstrukturen
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Bitte geben Sie einen Satz ein!");
+			string input = Console.ReadLine();
+			string[] words = input.Split(' ');
+			int wordCount = words.Length;
+			Console.WriteLine($"Position längstes Word :{wordCount}");
+
+			string longestWord = "";
+			foreach (string word in words)
+			{
+				if (word.Length > longestWord.Length)
+				{
+					longestWord = word;
+				}
+			}
+			Console.WriteLine($"Länge des längsten Wortes: {longestWord.Length}");
+			Console.WriteLine($"Längstes Wort: {longestWord}");
+		}
+	}
+}
