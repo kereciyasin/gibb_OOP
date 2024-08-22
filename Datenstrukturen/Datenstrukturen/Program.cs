8cc174cfabe23b831d81f5143603cb2338f582ba
Yasin Kereci <kereciyasin52@gmail.com>
2024-08-22 13:48:39 +0200

    finished

--- Datenstrukturen/Datenstrukturen/Program.cs
+++ Datenstrukturen/Datenstrukturen/Program.cs
@@ -4,20 +4,26 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Willkommen beim Lotto-Simulator!");	
+			Console.WriteLine("Willkommen beim Lotto-Simulator!");
+
+			// Kullaniciya 6 adet sayi girmesini soyle
 			int[] benutzerTipps = new int[6];
+
+			// 6 adet sayi cek
 			int[] gezogeneZahlen = new int[6];
-			
+
+
+			// Rastgele sayi uretme
 			Random zufall = new Random();
 
-			// Benutzer-Tipps abfragen
+			// Döngü ile kullanıcıdan sayıları al
 			for (int i = 0; i < benutzerTipps.Length; i++)
 			{
 				Console.Write($"Tipp {i + 1}: ");
 				benutzerTipps[i] = int.Parse(Console.ReadLine());
 			}
 
-			// Zufällige Zahlen ziehen
+			// Rastgele sayıları oluştur
 			for (int i = 0; i < gezogeneZahlen.Length; i++)
 			{
 				gezogeneZahlen[i] = zufall.Next(1, 46); // Zahlen zwischen 1 und 45 generieren
