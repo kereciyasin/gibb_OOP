dff0edb1427808aa292f548567ef98c12a1a4c3f
Yasin Kereci <kereciyasin52@gmail.com>
2024-08-23 11:35:34 +0200

    aufgabe finished

---
+++ Math und String Methoden nutzen/Math und String Methoden nutzen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Math_und_String_Methoden_nutzen
+{
+	internal class Program
+	{
+		// bana bir method yaz sayinin karakökünü alacak
+		static double BerechneWurzel(double zahl)
+		{
+			return Math.Sqrt(zahl);
+		}
+
+		static void Main(string[] args)
+		{
+			
+			
+			Console.WriteLine("Geben Sie eine Zahl ein: ");
+			int num = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Die Wurzel von " + num + " ist " + BerechneWurzel(num));
+			Console.ReadLine();	
+		}
+	}
+}
