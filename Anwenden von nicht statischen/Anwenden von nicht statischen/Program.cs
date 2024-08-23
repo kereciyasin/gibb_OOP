1ecae3acfb3c3adee589467b30a70b7953dcd0ca
Yasin Kereci <kereciyasin52@gmail.com>
2024-08-23 12:43:09 +0200

    list erstelt

--- Anwenden von nicht statischen/Anwenden von nicht statischen/Program.cs
+++ Anwenden von nicht statischen/Anwenden von nicht statischen/Program.cs
@@ -2,6 +2,24 @@
 {
 	internal class Program
 	{
+		// HelloSay methodunu olustur
+		public static void HelloSay(string name, string language)
+		{
 
+		}
+
+		static void Main(string[] args)
+		{
+			// person clasini list ile kullanma
+			List<Person> people = new List<Person>
+			{
+				new Person { Name = "Ahmet", Language = "Turkish" },
+				new Person { Name = "John", Language = "English" },
+				new Person { Name = "Ali", Language = "Arabic" }
+			};
+
+	
+
+		}
 	}
 }
