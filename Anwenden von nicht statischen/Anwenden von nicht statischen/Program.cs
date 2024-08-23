e4baea61e486b0ee4e846159bfc85cd96d090791
Yasin Kereci <kereciyasin52@gmail.com>
2024-08-23 12:43:41 +0200

    projekt finished

--- Anwenden von nicht statischen/Anwenden von nicht statischen/Program.cs
+++ Anwenden von nicht statischen/Anwenden von nicht statischen/Program.cs
@@ -5,7 +5,24 @@
 		// HelloSay methodunu olustur
 		public static void HelloSay(string name, string language)
 		{
-
+			// Eger language Turkish ise
+			if (language == "Turkish")
+			{
+				// Merhaba name yaz
+				Console.WriteLine("Merhaba " + name);
+			}
+			// Eger language English ise
+			else if (language == "English")
+			{
+				// Hello name yaz
+				Console.WriteLine("Hello " + name);
+			}
+			// Eger language Arabic ise
+			else if (language == "Arabic")
+			{
+				// Ahlan name yaz
+				Console.WriteLine("Ahlan " + name);
+			}
 		}
 
 		static void Main(string[] args)
@@ -18,7 +35,17 @@
 				new Person { Name = "Ali", Language = "Arabic" }
 			};
 
-	
+			// people listesindeki her bir kisinin adi ve dili icin HelloSay methodunu cagir	
+			foreach (var person in people)
+			{
+				HelloSay(person.Name, person.Language);
+			}
+
+
+
+			// Bekle
+			Console.ReadKey();
+
 
 		}
 	}
