013e07cdccce5d3b7861a29295734a5f33b01807
Yasin Kereci <kereciyasin52@gmail.com>
2024-08-24 10:53:04 +0200

    added

--- String Methoden nutzen/String Methoden nutzen/Program.cs
+++ String Methoden nutzen/String Methoden nutzen/Program.cs
@@ -7,31 +7,40 @@
 			Console.WriteLine("Bitte geben Sie Ihre Email ein: ");
 			string email = Console.ReadLine();
 
-			// eger @ eksikse
-			if (!email.Contains("@"))
+			do
 			{
-				Console.WriteLine("Es gibt kein @ Zeichen!");
-				return;
-			}
-			// eger kullanici adi eksikse
-			if (email.IndexOf("@") == 0)
-			{
-				Console.WriteLine("Es gibt kein Benutzername!");
-				return;
-			}
-			//her domain eksikse
-			if (email.IndexOf("@") == email.Length - 1)
-			{
-				Console.WriteLine("Es gibt kein Domain!");
-				return;
-			}
-			// eger domain uzantisi eksikse
-			if (!email.Contains(".com"))
-			{
-				Console.WriteLine("Domäne nicht korrekt");
-				return;
-			}
-			Console.WriteLine("Email ist korrekt!");
+				
+
+				// eger @ eksikse
+				if (!email.Contains("@"))
+				{
+					Console.WriteLine("Es gibt kein @ Zeichen!");
+					continue;
+				}
+				// eger kullanici adi eksikse
+				if (email.IndexOf("@") == 0)
+				{
+					Console.WriteLine("Es gibt kein Benutzername!");
+				continue;
+				}
+				//her domain eksikse
+				if (email.IndexOf("@") == email.Length - 1)
+				{
+					Console.WriteLine("Es gibt kein Domain!");
+					continue;
+				}
+				// eger domain uzantisi eksikse
+				if (!email.Contains(".com"))
+				{
+					Console.WriteLine("Domäne nicht korrekt");
+					continue;
+				}
+				
+
+			} while (email != "Email ist korrekt!");
+
+			
+			Console.ReadKey();
 
 		}
 	}
